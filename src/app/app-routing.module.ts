9a17a262b97bde79ec6051fca46965e756c58ccb
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-05-12 23:58:31 +0530

    Added content to github-user component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GithubUserComponent } from './github-user/github-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'user', component: GithubUserComponent},
+  {path: '**', redirectTo: 'user'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/github-user/github-user.component.ts
+++ src/app/github-user/github-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-github-user',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github-user.component.css']
 })
 export class GithubUserComponent implements OnInit {
+  user: any
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUserDetails (){
+    this.http.get('https://api.github.com/users/pankajparkar').subscribe(
+      user => this.user = user
+    )
+  }
 
   ngOnInit() {
   }
