80d5891d6b5d8efa5678378987fa6c1c0d72302b
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-05-16 22:24:32 +0530

    Added repos routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GithubUserComponent } from './github-user/github-user.component';
+import { GithubRepositoryComponent } from './github-repository/github-repository.component';
 
 const routes: Routes = [
   {path: 'user', component: GithubUserComponent},
+  {path: 'repos', component: GithubRepositoryComponent},
   {path: '**', redirectTo: 'user'}
 ];
 
