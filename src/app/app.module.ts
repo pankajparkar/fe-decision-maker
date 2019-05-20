c22e160ac7c843d7504a1f231dae5f8b2c10fce4
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-05-20 23:57:43 +0530

    Added navbar component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { GithubUserComponent } from './github-user/github-user.component';
 import { GithubSearchRepositoryComponent } from './github-search-repository/github-search-repository.component';
 import { NpmSearchRepositoryComponent } from './npm-search-repository/npm-search-repository.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     GithubRepositoryComponent,
     GithubUserComponent,
     GithubSearchRepositoryComponent,
-    NpmSearchRepositoryComponent
+    NpmSearchRepositoryComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
