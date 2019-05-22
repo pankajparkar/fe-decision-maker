eeb71b9e7bf5d0ad734df072ccd42ba0ffc13a32
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-05-22 23:57:55 +0530

    Added mat-list instead of ul-li

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { MatToolbarModule } from '@angular/material';
+import { MatToolbarModule, MatIconModule, MatListModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -29,7 +29,9 @@ import { NavbarComponent } from './navbar/navbar.component';
     BrowserAnimationsModule,
 
     // Material module 
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
